73c282ecb01ef1d5ef477d64ac72b843a6ed4ee3
Eli <104906733+Bonnet2B1@users.noreply.github.com>
2023-10-17 23:51:03 +0200

    start cpp: cpp/ex00: ok, cpp00/ex01: working on

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edelarbr <edelarbr@student.42mulhouse.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/17 23:11:08 by edelarbr          #+#    #+#             */
+/*   Updated: 2023/10/17 23:37:33 by edelarbr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	for (int i = 1; i < argc; i++)
+	{
+		for (int j = 0; argv[i][j]; j++)
+		{
+			if (argv[i][j] >= 'a' && argv[i][j] <= 'z')
+				std::cout << (char)toupper(argv[i][j]);
+			else
+				std::cout << argv[i][j];
+		}
+	}
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	std::cout << '\n';
+}
---
+++ cpp00/ex01/phonebook.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phonebook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edelarbr <edelarbr@student.42mulhouse.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/17 23:28:59 by edelarbr          #+#    #+#             */
+/*   Updated: 2023/10/17 23:46:40 by edelarbr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+class PhoneBook
+{
+	private:
+	public:
+}
+
+class Contact
+{
+	private:
+		std::string	_name;
+		int			_phone_num;
+	public:
+}
