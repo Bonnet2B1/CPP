d2e9bd7fd2f86c9c511d6e259bb335b9c2f46988
Eli <104906733+Bonnet2B1@users.noreply.github.com>
2023-10-22 16:25:29 +0200

    start C++01/ex05

---
+++ C++01/ex05/Harl.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edelarbr <edelarbr@student.42mulhouse.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/22 00:33:01 by edelarbr          #+#    #+#             */
+/*   Updated: 2023/10/22 01:01:41 by edelarbr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+# include <iostream>
+
+# define DEBUG_MSG "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!"
+# define INFO_MSG "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!"
+# define WARNING_MSG "I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month."
+# define ERROR_MSG "This is unacceptable! I want to speak to the manager now."
+
+class Harl
+{
+public:
+	Harl() {}
+	~Harl() {}
+	void complain( std::string level );
+private:
+	void debug( void );
+	void info( void );
+	void warning( void );
+	void error( void );
+};
+
+#endif
---
+++ C++01/ex05/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edelarbr <edelarbr@student.42mulhouse.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/22 00:42:18 by edelarbr          #+#    #+#             */
+/*   Updated: 2023/10/22 00:58:21 by edelarbr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int main(void)
+{
+	Harl harl;
+
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+
+	return (0);
+}
