23c55c9c4e3040f1b9c209e3cc9f3d701d091fbd
Bonnet2B1 <elias.delarbre@gmail.com>
2023-11-30 20:28:42 +0100

    format code

--- C++04/ex03/classes/ICharacter/ICharacter.hpp
+++ C++04/ex03/classes/ICharacter/ICharacter.hpp
@@ -6,7 +6,7 @@
 /*   By: edelarbr <edelarbr@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 17:07:38 by edelarbr          #+#    #+#             */
-/*   Updated: 2023/11/30 01:34:16 by edelarbr         ###   ########.fr       */
+/*   Updated: 2023/11/30 20:23:30 by edelarbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,10 @@ class ICharacter {
 
 public:
 	virtual ~ICharacter() {}
-	virtual std::string const &getName() const = 0;
-	virtual void equip(AMateria *m) = 0;
+	virtual std::string const & getName() const = 0;
+	virtual void equip(AMateria* m) = 0;
 	virtual void unequip(int idx) = 0;
-	virtual void use(int idx, ICharacter &target) = 0;
+	virtual void use(int idx, ICharacter& target) = 0;
 };
 
 #endif // ICHARACTER_HPP
