a29c49bd5983e87123b194db351b037ab8d0d03a
Eli <elias.delarbre@gmail.com>
2024-02-08 20:16:31 +0100

    C++05/ex00 finito pepito

--- C++05/ex00/Bureaucrat.cpp
+++ C++05/ex00/Bureaucrat.cpp
@@ -1,8 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edelarbr <edelarbr@student.42mulhouse.fr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/08 16:12:50 by edelarbr          #+#    #+#             */
+/*   Updated: 2024/02/08 16:17:20 by edelarbr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Bureaucrat.hpp"
 
 Bureaucrat::Bureaucrat() {}
 
-Bureaucrat::Bureaucrat(const std::string name) : _name(name) {}
+Bureaucrat::Bureaucrat(const std::string name) : _name(name) {
+	this->_grade = 150;
+}
 
 Bureaucrat::Bureaucrat(const Bureaucrat &src) {
 	*this = src;
@@ -12,7 +26,36 @@ Bureaucrat::~Bureaucrat() {}
 
 Bureaucrat &Bureaucrat::operator = (const Bureaucrat &rhs) {
 	if (this != &rhs) {
-		// - Copy all attributes here
+		*this = Bureaucrat(rhs.getName());
+		this->_grade = rhs._grade;
 	}
 	return (*this);
+}
+
+const std::string	Bureaucrat::getName() const {return _name;}
+
+int	Bureaucrat::getGrade() const {return _grade;}
+
+void	Bureaucrat::DecreaseGrade() {
+	if (this->_grade + 1 <= 150)
+		this->_grade++;
+	else
+		throw GradeTooHighException("The grade is too low");
+}
+
+void	Bureaucrat::IncreaseGrade() {
+	if (this->_grade - 1 >= 1)
+		this->_grade--;
+	else
+		throw GradeTooLowException("The grade is too high");
+}
+
+Bureaucrat::GradeTooHighException::GradeTooHighException(const char* error) : _error(error) {}
+
+Bureaucrat::GradeTooLowException::GradeTooLowException(const char* error) : _error(error) {}
+
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &rhs)
+{
+	out << rhs.getName() << ", bureaucrat grade " << rhs.getGrade() << ".";
+	return (out);
 }
--- C++05/ex00/main.cpp
+++ C++05/ex00/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: edelarbr <edelarbr@student.42mulhouse.f    +#+  +:+       +#+        */
+/*   By: edelarbr <edelarbr@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 16:29:26 by edelarbr          #+#    #+#             */
-/*   Updated: 2023/11/15 16:29:47 by edelarbr         ###   ########.fr       */
+/*   Updated: 2024/02/08 16:12:14 by edelarbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,28 @@
 
 int main() {
 
+	Bureaucrat B1("Charles");
+
+	try
+	{
+		B1.DecreaseGrade();
+	}
+	catch (std::exception & e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	std::cout << B1 << std::endl;
+
+	for (int i = 0; i != 150; i++)
+	{
+		try
+		{
+			B1.IncreaseGrade();
+		}
+		catch (std::exception & e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+	}
+	std::cout << B1 << std::endl;
 }
