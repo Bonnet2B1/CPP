bed31d2365830ccf6f0a31f5f8d99dfe11f9a28e
Eli <elias.delarbre@gmail.com>
2024-03-05 22:21:57 +0100

    starting C++08

---
+++ C++08/ex00/ft.h
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edelarbr <edelarbr@student.42mulhouse.fr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/05 19:04:54 by edelarbr          #+#    #+#             */
+/*   Updated: 2024/03/05 19:04:55 by edelarbr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void ft_putchar(char c);
+void ft_swap(int *a, int *b);
+void ft_putstr(char *str);
+int ft_strlen(char *str);
+int ft_strcmp(char *s1, char *s2);
