55734a676a20b24ca6f06b809197595daaac2ec4
Bonnet2B1 <elias.delarbre@gmail.com>
2024-03-06 17:40:58 +0100

    save progress

--- C++08/ex00/easyfind.hpp
+++ C++08/ex00/easyfind.hpp
@@ -6,11 +6,27 @@
 /*   By: edelarbr <edelarbr@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 10:27:41 by edelarbr          #+#    #+#             */
-/*   Updated: 2024/03/06 11:39:55 by edelarbr         ###   ########.fr       */
+/*   Updated: 2024/03/06 17:09:41 by edelarbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include <iostream>
+# include <vector>
+
 template <typename T>
-void	easyfind(T container, int nbr) {
-	
+typename T::value_type easyfind(T& container, int nbr)
+{
+	typename T::iterator it = container.begin();
+	while (it != container.end())
+	{
+		if (*it == nbr)
+			return *it;
+		++it;
+	}
+	throw std::exception();
 }
+
+#endif
