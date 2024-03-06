e069013c871bbe74e18e3eefbd23d4b08f3a27e6
Eli <elias.delarbre@gmail.com>
2024-03-06 20:00:44 +0100

    finito pepito

--- C++08/ex00/main.cpp
+++ C++08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: edelarbr <edelarbr@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 12:50:37 by edelarbr          #+#    #+#             */
-/*   Updated: 2024/03/06 17:04:04 by edelarbr         ###   ########.fr       */
+/*   Updated: 2024/03/06 19:58:36 by edelarbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@
 int main()
 {
 	int tab[] = {3, 2, 3};
-	for (int i = 0; i < 3; i++)
 	std::vector<int> vec(tab, tab + sizeof(tab) / sizeof(tab[0]));
 	std::cout << easyfind(vec, 2) << std::endl;
 }
