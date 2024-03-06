c475cd01ce73afdd0c78812228f48099e2b8b654
Eli <elias.delarbre@gmail.com>
2024-03-06 11:47:53 +0100

    En fait je faisais le C08 et pas le C++08 hehe

--- C++08/ex00/easyfind.hpp
+++ C++08/ex00/easyfind.hpp
@@ -1,17 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft.h                                               :+:      :+:    :+:   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: edelarbr <edelarbr@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/05 19:04:54 by edelarbr          #+#    #+#             */
-/*   Updated: 2024/03/05 19:04:55 by edelarbr         ###   ########.fr       */
+/*   Created: 2024/03/06 10:27:41 by edelarbr          #+#    #+#             */
+/*   Updated: 2024/03/06 11:39:55 by edelarbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void ft_putchar(char c);
-void ft_swap(int *a, int *b);
-void ft_putstr(char *str);
-int ft_strlen(char *str);
-int ft_strcmp(char *s1, char *s2);
+template <typename T>
+void	easyfind(T container, int nbr) {
+	
+}
